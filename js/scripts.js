16a57c66fdc422c907b724ad06c35f50a7b2f01d
Ewelina <ewelkarewelka@gazeta.pl>
2017-11-21 13:19:25 +0100

    Zadanie8.6

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+
+// scripts.js
+
+var name = prompt('Enter your name');
+
+// comment 1 
+alert('Hello, ' + name);
+
+// comment 2
+console.log('Hello, ' + name);
+
+// zadanie 8.6
+
+var a = 7
+var b = 8
+var value = (a * a) - (2 * a * b) + (b * b) 
+console.log(value)
+
+if(value>0) {
+console.log("Wynik jest dodatni");
+}
+else if(value<0) {
+console.log("Wynik jest ujemny");
+}
+else if(value==0) {
+console.log("Wynik jest równy 0");
+}
+
+ 
+
+
